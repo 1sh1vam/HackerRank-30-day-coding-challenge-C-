568498c37f74788bebe627e1cf544a0e7434837d
Shivam Mishra <56019984+1sh1vam@users.noreply.github.com>
2021-04-18 12:10:06 +0530

    Add files via upload

---
+++ challenge5.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+    
+//     for(int j=n-1; j>=0; j--){
+//         cout << arr[j] <<" ";
+//     }
+
+//     return 0;
+// }
+
+
+int main(){
+    cout << (-4 > -6);
+    return 0;
+}
